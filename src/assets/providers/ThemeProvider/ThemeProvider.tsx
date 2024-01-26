345b02391b9cb67ba086c880820e4e2604b39eb1
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2024-01-26 19:08:05 +0300

    install tauri api

--- src/assets/providers/ThemeProvider/ThemeProvider.tsx
+++ src/assets/providers/ThemeProvider/ThemeProvider.tsx
@@ -2,11 +2,15 @@ import { PropsWith } from '@xenopomp/advanced-types';
 
 import { FC, useContext, useEffect } from 'react';
 
+import { useAppSelector } from '@redux/hooks';
+
 import { BodyClassnameContext } from '@providers/BodyClassnameProvider/BodyClassnameProvider';
 
 import styles from './ThemeProvider.module.scss';
 
 const ThemeProvider: FC<PropsWith<'children', {}>> = ({ children }) => {
+  const theme = useAppSelector(state => state.appSettings.theme);
+
   const classContext = useContext(BodyClassnameContext);
 
   useEffect(() => {
