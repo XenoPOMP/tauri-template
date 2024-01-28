c4aa3411bc9b5d07622d9a41fe68b5b136072620
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2024-01-28 14:10:24 +0300

    create useNotifications hook

---
+++ src/assets/hooks/useNotifications.ts
@@ -0,0 +1,44 @@
+import { ReplaceReturnType } from '@xenopomp/advanced-types';
+
+import {
+  isPermissionGranted,
+  requestPermission,
+  sendNotification,
+} from '@tauri-apps/api/notification';
+
+interface ITauriNotificationsHook {
+  sendNotification: ReplaceReturnType<typeof sendNotification, Promise<void>>;
+  isPermissionGranted: typeof isPermissionGranted;
+  requestPermission: typeof requestPermission;
+}
+
+/**
+ * This hook allows to use Tauri`s api to send notifications.
+ *
+ * @example
+ * const { sendNotification } = useNotifications();
+ *
+ * // Function checks if access is granted automatically.
+ * sendNotification('Tauri is awesome!');
+ * sendNotification({ title: 'TAURI', body: 'Tauri is awesome!' });
+ */
+export const useNotifications = (): ITauriNotificationsHook => {
+  const send: ITauriNotificationsHook['sendNotification'] = async options => {
+    let permissionGranted = await isPermissionGranted();
+
+    if (!permissionGranted) {
+      const permission = await requestPermission();
+      permissionGranted = permission === 'granted';
+    }
+
+    if (permissionGranted) {
+      sendNotification(options);
+    }
+  };
+
+  return {
+    sendNotification: send,
+    isPermissionGranted,
+    requestPermission,
+  };
+};
