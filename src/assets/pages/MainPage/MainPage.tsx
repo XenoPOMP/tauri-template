16dd6a88a005d9cd36840ef94ef25ed496d8b26e
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2024-01-28 14:17:43 +0300

    demonstrate notification API

--- src/assets/pages/MainPage/MainPage.tsx
+++ src/assets/pages/MainPage/MainPage.tsx
@@ -2,9 +2,13 @@ import cn from 'classnames';
 
 import Page from '@components/Page/Page';
 
+import { useNotifications } from '@hooks/useNotifications';
+
 import styles from './MainPage.module.scss';
 
 const MainPage = () => {
+  const { sendNotification } = useNotifications();
+
   return (
     <Page
       meta={{
@@ -12,7 +16,19 @@ const MainPage = () => {
         description: 'Some description',
         keywords: '',
       }}
-    ></Page>
+    >
+      <h1 className={cn('font-bold text-2xl')}>Notification API</h1>
+      <button
+        onClick={() => {
+          let ignore = sendNotification({
+            title: 'Tauri app template',
+            body: 'You got 1 message from app.',
+          });
+        }}
+      >
+        Send notification
+      </button>
+    </Page>
   );
 };
 
